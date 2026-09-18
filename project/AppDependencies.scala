d999042aeb7802b478aa8db8badb06780b95125c
Luke Jones <47660116+BetaDraconis@users.noreply.github.com>
2026-09-18 10:14:32 +0100

    LEPP-647: Update dependencies (#19)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
   private val playVersion = "play-30"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongoVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.13.0",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.22.2",
     "uk.gov.hmrc"                   %% s"sca-wrapper-$playVersion"  % "6.3.0",
     "com.softwaremill.retry"        %% "retry"                      % "0.3.6",
   )
